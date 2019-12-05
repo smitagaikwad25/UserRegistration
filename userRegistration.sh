db79d6ca7878a920035f4de1405ce19354c56e35
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-05 16:15:16 +0530

    Add : validate First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 echo "WelCome in user registration"
 
+function validateFirstName()
+{
+	echo "enter your first name"
+	read firstName
+	pattern="^[A-Z]{1}[a-z]{2}$"
+	if [[ $firstName =~ $pattern ]]
+	then
+		echo "Name is valid"
+	else
+		echo "Name is invalid"
+	fi
+}
+function main()
+{
+	validateFirstName
+}
+main
+
