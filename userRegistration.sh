fcfdc245d216238af60bb6d3961c1f4b57a8f655
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-05 16:30:18 +0530

    Add : validate Last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,7 +3,7 @@ echo "WelCome in user registration"
 
 function validateFirstName()
 {
-	echo "enter your first name"
+	echo "enter your first name :"
 	read firstName
 	pattern="^[A-Z]{1}[a-z]{2}$"
 	if [[ $firstName =~ $pattern ]]
@@ -13,9 +13,26 @@ function validateFirstName()
 		echo "Name is invalid"
 	fi
 }
+
+function  validateLastName()
+{
+	echo "enter your last name :"
+	read lastName
+	lastnamePattern="^[A-Z]{1}[a-z]{2}$"
+	if [[ $lastName =~ $pattern ]]
+	then
+		echo "last name is valid"
+	else
+		echo "last name is invalid"
+	fi
+
+
+}
+
 function main()
 {
 	validateFirstName
+	validateLastName
 }
 main
 
